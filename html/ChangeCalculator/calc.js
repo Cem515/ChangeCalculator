cf74548b6ea745698af2559cdaa28984f875b0aa
Collin McCoy <collinmccoy17@gmail.com>
2017-04-25 14:52:26 -0700

    changes to function

--- html/ChangeCalculator/calc.js
+++ html/ChangeCalculator/calc.js
@@ -4,8 +4,13 @@ $(document).ready(function() {
     var cost = $("#totalCost").val();
     var given = $("#amountGiven").val();
     var change =(given - cost).toFixed(2);
-
+    var dollars = Math.floor(given-cost);
+    var remainder = (change-cost);
     document.getElementById('change').innerHTML = "Customer's Change is " + change;
-
+    document.getElementById('dollars').innerHTML = "Dollars = "+ dollars;
+    alert(remainder);
   })
 });
+
+
+//res = x % y;
