47a0f5d589087c80f1469ae4a0ad71cc2316669e
Collin McCoy <collinmccoy17@gmail.com>
2017-04-25 15:14:31 -0700

    working Calcuator

--- html/ChangeCalculator/calc.js
+++ html/ChangeCalculator/calc.js
@@ -5,10 +5,20 @@ $(document).ready(function() {
     var given = $("#amountGiven").val();
     var change =(given - cost).toFixed(2);
     var dollars = Math.floor(given-cost);
-    var remainder = (change-cost);
+    var remainder = ((change-dollars)*100).toFixed(0);
+    var quarters = Math.floor(remainder/25).toFixed(0);
+    remainder %= 25;
+    var dimes = Math.floor(remainder/10).toFixed(0);
+    remainder %=10;
+    var nickels = Math.floor(remainder/5).toFixed(0);
+    remainder %=5;
+    var pennies = remainder
     document.getElementById('change').innerHTML = "Customer's Change is " + change;
     document.getElementById('dollars').innerHTML = "Dollars = "+ dollars;
-    alert(remainder);
+    document.getElementById('quarters').innerHTML = "Quarters = " +quarters;
+    document.getElementById('dimes').innerHTML = "Dimes = "+dimes;
+    document.getElementById('nickels').innerHTML = "Nickels = "+nickels;
+    document.getElementById('pennies').innerHTML = "Pennies = "+pennies;
   })
 });
 
