003568a8bfd83096adb2df20a0ca254ba33f84ff
Collin McCoy <collinmccoy17@gmail.com>
2017-04-25 14:36:38 -0700

    working calculator

--- html/ChangeCalculator/calc.js
+++ html/ChangeCalculator/calc.js
@@ -1,18 +1,11 @@
 $(document).ready(function() {
   $("button").click(function() {
 
-    var firstCost = $("#totalCost").val()
-    var cost = Math.round(firstCost).toFixed(2)
-    var firstGiven = $("#amountGiven").val();
-    var given = Math.round(firstGiven).toFixed(2)
-    var change = Math.round(given - cost).toFixed(2);
+    var cost = $("#totalCost").val();
+    var given = $("#amountGiven").val();
+    var change =(given - cost).toFixed(2);
 
     document.getElementById('change').innerHTML = "Customer's Change is " + change;
 
   })
 });
-
-
-/*var singleValues = $( "#single" ).val();
-$( "p" ).click(function() {
-  $( this ).slideUp();*/
