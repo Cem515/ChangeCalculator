d43abb0fc3a3ac92f1c64b9761a6d7cfdeeef052
Collin McCoy <collinmccoy17@gmail.com>
2017-04-25 13:38:19 -0700

    input functions

--- html/ChangeCalculator/calc.js
+++ html/ChangeCalculator/calc.js
@@ -1,5 +1,18 @@
-$( document ).ready() {
+$(document).ready(function() {
+  $("button").click(function() {
 
+    var firstCost = $("#totalCost").val()
+    var cost = Math.round(firstCost).toFixed(2)
+    var firstGiven = $("#amountGiven").val();
+    var given = Math.round(firstGiven).toFixed(2)
+    var change = Math.round(given - cost).toFixed(2);
 
+    document.getElementById('change').innerHTML = "Customer's Change is " + change;
 
+  })
 });
+
+
+/*var singleValues = $( "#single" ).val();
+$( "p" ).click(function() {
+  $( this ).slideUp();*/
