de0114adba16058cf96a741e176029bdcded733e
Collin McCoy <collinmccoy17@gmail.com>
2017-04-26 10:05:12 -0700

    border interface

--- html/ChangeCalculator/calc.js
+++ html/ChangeCalculator/calc.js
@@ -1,36 +1,64 @@
 $(document).ready(function() {
-  $("button").click(function() {
+      $("button").click(function() {
+            //Return Two Inputed Values as Numbers
+            var cost = $("#totalCost").val();
+            var given = $("#amountGiven").val();
 
-    var cost = $("#totalCost").val();
-    var given = $("#amountGiven").val();
-    var change = (given - cost).toFixed(2);
-    var dollars = Math.floor(given - cost);
-    var remainder = ((change - dollars) * 100).toFixed(0);
-    var quarters = Math.floor(remainder / 25).toFixed(0);
-    remainder %= 25;
-    var dimes = Math.floor(remainder / 10).toFixed(0);
-    remainder %= 10;
-    var nickels = Math.floor(remainder / 5).toFixed(0);
-    remainder %= 5;
-    var pennies = remainder;
+            //Start with Total Change
+            var change = (given - cost).toFixed(2);
+            //Begin Removing Proper Money Denominations
+            var dollars = Math.floor(given - cost);
+            var remainder = ((change - dollars) * 100).toFixed(0);
+            var quarters = Math.floor(remainder / 25).toFixed(0);
+            remainder %= 25;
+            var dimes = Math.floor(remainder / 10).toFixed(0);
+            remainder %= 10;
+            var nickels = Math.floor(remainder / 5).toFixed(0);
+            remainder %= 5;
+            var pennies = remainder;
+            //Output Values to innerHTML
 
-        $("#change").html("Customer's Change is " + change);
-        $("#dollars").html("Dollars = "+ dollars);
-        $("#quarters").html("Quarters = "+ quarters);
-        $("#dimes").html("Dimes = "+ dimes);
-        $("#nickels").html("Nickels = "+ nickels);
-        $("#pennies").html("Pennies = "+ pennies);
-
-/*
-    document.getElementById('change').innerHTML = "Customer's Change is " + change;
-    document.getElementById('dollars').innerHTML = "Dollars = " + dollars;
-    document.getElementById('quarters').innerHTML = "Quarters = " + quarters;
-    document.getElementById('dimes').innerHTML = "Dimes = " + dimes;
-    document.getElementById('nickels').innerHTML = "Nickels = " + nickels;
-    document.getElementById('pennies').innerHTML = "Pennies = " + pennies;
-*/
-  })
-});
+            $("#change").html("Customer's Change is " + change);
+            $("#dollars").html("Dollars = " + dollars);
+            $("#quarters").html("Quarters = " + quarters);
+            $("#dimes").html("Dimes = " + dimes);
+            $("#nickels").html("Nickels = " + nickels);
+            $("#pennies").html("Pennies = " + pennies);
 
 
-//res = x % y;
+            // Hover Border Function
+            $("#change").hover(function() {
+                  $(this).addClass("Hover");},
+                  function() {$(this).removeClass("Hover");
+                })
+                $("#dollars").hover(
+                  function() {$(this).addClass("Hover");
+                  },
+                  function() { $(this).removeClass("Hover");
+                  })
+                  $("#quarters").hover(
+                    function() {$(this).addClass("Hover");
+                    },
+                    function() {$(this).removeClass("Hover");
+                    })
+                    $("#dimes").hover(
+                      function() {$(this).addClass("Hover");
+                      },
+                      function() {$(this).removeClass("Hover");
+                      })
+                      $("#nickels").hover(
+                        function() {$(this).addClass("Hover");
+                        },
+                        function() {$(this).removeClass("Hover");
+                        })
+                        $("#pennies").hover(
+                          function() { $(this).addClass("Hover");
+                          },
+                          function() {$(this).removeClass("Hover");
+                          })
+                      })
+                  });
+
+
+                //   $( "div" ).first().show( "fast", function showNext() {
+                //    $( this ).next( "div" ).show( "fast", showNext );
