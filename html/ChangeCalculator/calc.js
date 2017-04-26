622b8defa23df4d5b89b81117689fe6b72967d81
Collin McCoy <collinmccoy17@gmail.com>
2017-04-25 17:19:10 -0700

    updated with style

--- html/ChangeCalculator/calc.js
+++ html/ChangeCalculator/calc.js
@@ -3,22 +3,32 @@ $(document).ready(function() {
 
     var cost = $("#totalCost").val();
     var given = $("#amountGiven").val();
-    var change =(given - cost).toFixed(2);
-    var dollars = Math.floor(given-cost);
-    var remainder = ((change-dollars)*100).toFixed(0);
-    var quarters = Math.floor(remainder/25).toFixed(0);
+    var change = (given - cost).toFixed(2);
+    var dollars = Math.floor(given - cost);
+    var remainder = ((change - dollars) * 100).toFixed(0);
+    var quarters = Math.floor(remainder / 25).toFixed(0);
     remainder %= 25;
-    var dimes = Math.floor(remainder/10).toFixed(0);
-    remainder %=10;
-    var nickels = Math.floor(remainder/5).toFixed(0);
-    remainder %=5;
-    var pennies = remainder
+    var dimes = Math.floor(remainder / 10).toFixed(0);
+    remainder %= 10;
+    var nickels = Math.floor(remainder / 5).toFixed(0);
+    remainder %= 5;
+    var pennies = remainder;
+    /*
+        $("p #change").html("Customer's Change is " + change);
+        $("p #dollars").html("Dollars = "+ dollars);
+        $("p #quarters").html("Quarters = "+ quarters);
+        $("p #dimes").html("Dimes = "+ dimes);
+        $("p #nickels").html("Nickels = "+ nickels);
+        $("p #pennies").html("Pennies = "+ pennies);
+    */
+
     document.getElementById('change').innerHTML = "Customer's Change is " + change;
-    document.getElementById('dollars').innerHTML = "Dollars = "+ dollars;
-    document.getElementById('quarters').innerHTML = "Quarters = " +quarters;
-    document.getElementById('dimes').innerHTML = "Dimes = "+dimes;
-    document.getElementById('nickels').innerHTML = "Nickels = "+nickels;
-    document.getElementById('pennies').innerHTML = "Pennies = "+pennies;
+    document.getElementById('dollars').innerHTML = "Dollars = " + dollars;
+    document.getElementById('quarters').innerHTML = "Quarters = " + quarters;
+    document.getElementById('dimes').innerHTML = "Dimes = " + dimes;
+    document.getElementById('nickels').innerHTML = "Nickels = " + nickels;
+    document.getElementById('pennies').innerHTML = "Pennies = " + pennies;
+
   })
 });
 
